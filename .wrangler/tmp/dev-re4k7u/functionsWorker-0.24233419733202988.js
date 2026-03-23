d5d4f3f3283e0a19b17460fd11a8c82f7c1508a9
Rob McCormack <robmccormack@users.noreply.github.com>
2026-03-23 13:41:32 -0400

    Update wrangler.toml with production and local development instructions; remove obsolete middleware files to streamline project structure.

--- .wrangler/tmp/dev-re4k7u/functionsWorker-0.24233419733202988.js
+++ .wrangler/tmp/dev-re4k7u/functionsWorker-0.24233419733202988.js
@@ -850,7 +850,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-Phvtqy/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-2irz6o/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -882,7 +882,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-Phvtqy/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-2irz6o/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
